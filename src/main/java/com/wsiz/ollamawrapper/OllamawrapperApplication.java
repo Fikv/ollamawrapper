6262cc1dfaa7cd51e1713ceb0e0dff1ec0d1b57d
fikv <jakubfik21@gmail.com>
2024-11-06 17:45:40 +0100

    init

---
+++ src/main/java/com/wsiz/ollamawrapper/OllamawrapperApplication.java
@@ -0,0 +1,13 @@
+package com.wsiz.ollamawrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OllamawrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OllamawrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wsiz/ollamawrapper/OllamawrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsiz.ollamawrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OllamawrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
