bbbddd5362c5d75c91cec5ed958cc3ac4a55b380
Konrad Pelc <kpelc54@gmail.com>
2024-12-23 20:52:30 +0100

    Migracje

--- src/main/java/com/wsiz/ollamawrapper/database/PromptRepository.java
+++
@@ -1,8 +0,0 @@
-package com.wsiz.ollamawrapper.database;
-
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Repository
-public interface PromptRepository extends JpaRepository<Prompt, Long> {
-}
