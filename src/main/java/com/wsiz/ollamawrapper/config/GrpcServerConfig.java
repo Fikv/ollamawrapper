cd01493950098e581775bdf2bd0fb6006850d9f1
Konrad Pelc <kpelc54@gmail.com>
2024-12-27 15:37:42 +0100

    Dodanie SQLite z migracjami

--- src/main/java/com/wsiz/ollamawrapper/config/GrpcServerConfig.java
+++ src/main/java/com/wsiz/ollamawrapper/config/GrpcServerConfig.java
@@ -1,7 +1,12 @@
 package com.wsiz.ollamawrapper.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import com.wsiz.ollamawrapper.services.ChatService;
 
@@ -16,5 +21,18 @@ public class GrpcServerConfig {
 		return ServerBuilder.forPort(9090)
 							.addService(chatService)
 							.build();
+	}
+
+	@Autowired
+	Environment env;
+
+	@Bean
+	public DataSource dataSource() {
+		final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(env.getProperty("driverClassName"));
+		dataSource.setUrl(env.getProperty("url"));
+		dataSource.setUsername(env.getProperty("user"));
+		dataSource.setPassword(env.getProperty("password"));
+		return dataSource;
 	}
 }
