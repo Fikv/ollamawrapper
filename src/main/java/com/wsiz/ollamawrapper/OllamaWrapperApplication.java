c62f754996d8b4be8675e6cbad7c1f3f9754ce6e
Maciej Barć <xgqt@gentoo.org>
2025-01-30 00:09:53 +0100

    feat(ollamawrapper/OllamaWrapperApplication.java): run ollama on startup
    Signed-off-by: Maciej Barć <xgqt@gentoo.org>

--- src/main/java/com/wsiz/ollamawrapper/OllamaWrapperApplication.java
+++ src/main/java/com/wsiz/ollamawrapper/OllamaWrapperApplication.java
@@ -3,11 +3,16 @@ package com.wsiz.ollamawrapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.wsiz.ollamawrapper.ollamaexe.OllamaRunner;
+
 @SpringBootApplication
 public class OllamaWrapperApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OllamaWrapperApplication.class, args);
-	}
+    public static void main(final String[] args) {
+        final OllamaRunner ollamaRunner = new OllamaRunner();
+        ollamaRunner.makeOllamaThread();
+
+        SpringApplication.run(OllamaWrapperApplication.class, args);
+    }
 
 }
